cad675ab67de07210d1d182e97b422bc294d31e3
Mitali2700 <201B155@juetguna.in>
2022-07-26 23:12:16 +0530

    performing addition

--- calculator.js
+++ calculator.js
@@ -18,8 +18,14 @@ app.get("/", function(req,res){
 });
 
 app.post("/",function(req,res){
-    console.log(req.body.num1);
-    res.send("Thanks for posting that.")
+    // console.log(req.body.num1);
+    // res.send("Thanks for posting that.")
+
+    var num1 = req.body.num1;
+    var num2 = req.body.num2;
+
+    var result = num1+ num2;
+    res.send("The result of the calculation is "+ result);
 });
 
 app.listen(3000,function(){
