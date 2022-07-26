c1d91725b3e8eff8246b36a5c3bd9a3185723691
Mitali2700 <201B155@juetguna.in>
2022-07-26 23:27:38 +0530

    modification 2

--- calculator.js
+++ calculator.js
@@ -21,8 +21,12 @@ app.post("/",function(req,res){
     // console.log(req.body.num1);
     // res.send("Thanks for posting that.")
 
-    var num1 = req.body.num1;
-    var num2 = req.body.num2;
+    // var num1 = req.body.num1;
+    // var num2 = req.body.num2;
+    //  They will append the num1 and num2
+
+    var num1 = Number(req.body.num1);
+    var num2 = Number(req.body.num2);
 
     var result = num1+ num2;
     res.send("The result of the calculation is "+ result);
@@ -47,9 +51,13 @@ app.listen(3000,function(){
 // 500 - I screwed up
 
 /* 
-<H1 align="center">Calcuolator<H1>
+<H1 align="center">Calcuolator</H1>
 <H2>Technologies used</H2>
- 
+<a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg" alt="javascript" width="40" height="40"/> </a>
+<a href="https://nodejs.org/api/" target="_blank" rel="noreferrer"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Node.js_logo.svg/128px-Node.js_logo.svg.png" alt="Node.js" width="60"></a>
+<a href="https://www.npmjs.com/" target="_blank" rel="noreferrer"><img src="https://user-images.githubusercontent.com/78539161/180617223-47849d60-5538-43b7-8474-77d9db58728b.png" alt="npm" width="40"></a>
+<a href="https://expressjs.com/" target="_blank" rel="noreferrer"><img src="https://user-images.githubusercontent.com/78539161/180755429-687cd845-0559-4140-9069-4a453ac307b7.png" alt="Express.js" height="18"></a>
+<a href="https://hyper.is/" target="_blank" rel="noreferrer"><img src="https://user-images.githubusercontent.com/78539161/180262577-2538fca2-5701-4db4-a2fd-0871adc4af84.svg" width="40" height="40"></a>
 */
 
 /*
