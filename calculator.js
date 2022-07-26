63e0dd764add98cbd922c5dd517ef94f0dc0bc2b
Mitali2700 <201B155@juetguna.in>
2022-07-26 12:45:15 +0530

    creating path.js

--- calculator.js
+++ calculator.js
@@ -5,10 +5,14 @@ const express = require("express");
 const app= express();
 
 app.get("/", function(req,res){
-    res.send("Hello world");
-} );
+    res.sendFile(__dirname);
+});
 
 app.listen(3000,function(){
     console.log("Server is running on port 3000.");
 });
 
+// For sending individual bits of HTML data we use res.send
+// For sending entire webpage like "index.html" we use
+// go to: expressjs.com  then go to   API reference 4x 
+// here we get - res.sendFile 
