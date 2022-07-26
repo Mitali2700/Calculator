6e5e2727fabea2c4ce808fab717825495ca9fb7e
Mitali2700 <201B155@juetguna.in>
2022-07-26 15:11:22 +0530

    removing 404 error

--- calculator.js
+++ calculator.js
@@ -8,6 +8,10 @@ app.get("/", function(req,res){
     res.sendFile(__dirname+"/index.html");
 });
 
+app.post("/",function(req,res){
+    res.send("Thanks for posting that.")
+});
+
 app.listen(3000,function(){
     console.log("Server is running on port 3000.");
 });
@@ -16,3 +20,12 @@ app.listen(3000,function(){
 // For sending entire webpage like "index.html" we use
 // go to: expressjs.com  then go to   API reference 4x 
 // here we get - res.sendFile 
+
+
+// What is 404 not found
+// HTTP return code cheat sheet
+// 100 - Hold on
+// 200 - Here you go
+// 300 - Go away
+// 400 - You screwed up
+// 500 - I screwed up
