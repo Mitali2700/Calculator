a6a4154f3682888f7738a344490f9aaacaa3fb61
Mitali2700 <201B155@juetguna.in>
2022-07-29 00:17:38 +0530

    modification-4

--- calculator.js
+++ calculator.js
@@ -74,13 +74,6 @@ app.listen(3000,function(){
 // 500 - I screwed up
 
 /*
-<H1 align="center">Calculator <img src=https://user-images.githubusercontent.com/78539161/181079170-d5f38835-4dca-4664-88c3-4fe8c1215d49.png height="40"></H1>
-<H2>Technologies used</H2>
-<a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg" alt="javascript" width="40" height="40"/> </a>
-<a href="https://nodejs.org/api/" target="_blank" rel="noreferrer"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Node.js_logo.svg/128px-Node.js_logo.svg.png" alt="Node.js" width="60"></a>
-<a href="https://www.npmjs.com/" target="_blank" rel="noreferrer"><img src="https://user-images.githubusercontent.com/78539161/180617223-47849d60-5538-43b7-8474-77d9db58728b.png" alt="npm" width="40"></a>
-<a href="https://expressjs.com/" target="_blank" rel="noreferrer"><img src="https://user-images.githubusercontent.com/78539161/180755429-687cd845-0559-4140-9069-4a453ac307b7.png" alt="Express.js" height="18"></a>
-<a href="https://hyper.is/" target="_blank" rel="noreferrer"><img src="https://user-images.githubusercontent.com/78539161/180262577-2538fca2-5701-4db4-a2fd-0871adc4af84.svg" width="40" height="40"></a>
 
 */
 
@@ -88,7 +81,41 @@ app.listen(3000,function(){
 We need to be able to get that data into here, into this callback function, so we can calculate the output,and then 
 send the result back to the browser.Now, in order to tap into those pieces of data, we have to install another NPM 
 package, which is called Body Parser.So let's head over to our command line, and Command C to exit our server, and 
-then we're going to npm install - "npm install body-parser"
+then w<H1 align="center">Calculator <img src=https://user-images.githubusercontent.com/78539161/181079170-d5f38835-4dca-4664-88c3-4fe8c1215d49.png height="40"></H1>
+<H2>Technologies used</H2>
+<a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg" alt="javascript" width="40" height="40"/> </a>
+<a href="https://nodejs.org/api/" target="_blank" rel="noreferrer"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Node.js_logo.svg/128px-Node.js_logo.svg.png" alt="Node.js" width="60"></a>
+<a href="https://www.npmjs.com/" target="_blank" rel="noreferrer"><img src="https://user-images.githubusercontent.com/78539161/180617223-47849d60-5538-43b7-8474-77d9db58728b.png" alt="npm" width="40"></a>
+<a href="https://expressjs.com/" target="_blank" rel="noreferrer"><img src="https://user-images.githubusercontent.com/78539161/180755429-687cd845-0559-4140-9069-4a453ac307b7.png" alt="Express.js" height="18"></a>
+<a href="https://hyper.is/" target="_blank" rel="noreferrer"><img src="https://user-images.githubusercontent.com/78539161/180262577-2538fca2-5701-4db4-a2fd-0871adc4af84.svg" width="40" height="40"></a>
+
+
+<H2>Screen Shot: Calculator</H2>
+<H3>Screenshot 1</H3>
+
+![calc1](https://user-images.githubusercontent.com/78539161/181614673-0cb25279-8282-4ce9-9a9c-41a013f623df.png)
+
+<H3>Screenshot 2</H3>
+
+![calc2](https://user-images.githubusercontent.com/78539161/181614696-01665821-51c5-4d38-924a-a64c8ef4356d.png)
+
+<H3>Screenshot 3</H3>
+
+![calc3](https://user-images.githubusercontent.com/78539161/181614710-560f8ae3-b557-47a2-b1fa-96d56a6344d5.png)
+
+<H2>Screen Shot: BMI Calculator</H2>
+<H3>Screenshot 1</H3>
+
+![bmi1](https://user-images.githubusercontent.com/78539161/181614737-f44fa9db-171c-4e2f-b884-f99f3c57fe35.png)
+
+<H3>Screenshot 2</H3>
+
+![bmi2](https://user-images.githubusercontent.com/78539161/181614721-56c36f48-8609-46bb-9fb4-6d982576c084.png)
+
+<H3>Screenshot 3</H3>
+
+![bmi3](https://user-images.githubusercontent.com/78539161/181614795-8aab372f-974e-42eb-a264-7571b0552e11.png)
+e're going to npm install - "npm install body-parser"
 */
 
 // JSON File - 
